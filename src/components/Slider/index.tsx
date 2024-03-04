0713acbe017785916fb92f329af824fdcf153fea
Alan0602 <alankanil1234@gmail.com>
2024-03-04 22:30:31 +0530

    Set slides images

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -2,6 +2,22 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import styles from './index.module.css'
 
 const Slider = () => {
+
+    const slides = [
+        {
+          title: 'https://sngist.org/contents/wp-content/themes/sngist18/assets/img/slides/slide_1.jpg',
+          content: 'Content for Slide 1',
+        },
+        {
+          title: 'https://sngist.org/contents/wp-content/themes/sngist18/assets/img/slides/slide_3.jpg',
+          content: 'Content for Slide 2',
+        },
+        {
+          title: 'https://sngist.org/contents/wp-content/themes/sngist18/assets/img/slides/slide_4.jpg',
+          content: 'Content for Slide 3',
+        },
+      ];
+
   return (
     <>
     <div className={styles.sliderWrapper}>
@@ -10,12 +26,13 @@ const Slider = () => {
       slidesPerView={3}
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
+    >{slides.map((slide) => (
+
+        <SwiperSlide><img src={slide.title} alt={slide.content} /></SwiperSlide>
+    ))
+
+    }
+     
     </Swiper>
     </div>
     </>
