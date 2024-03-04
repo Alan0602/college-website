d4f4d6ca23b55588a1774c37896ee7176ce8aa9d
Alan0602 <alankanil1234@gmail.com>
2024-03-04 22:25:20 +0530

    New swiper added

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,10 +1,22 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
 import styles from './index.module.css'
 
 const Slider = () => {
   return (
     <>
     <div className={styles.sliderWrapper}>
-        
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      ...
+    </Swiper>
     </div>
     </>
   )
